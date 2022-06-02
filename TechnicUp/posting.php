c80a156e700f3e8d9f65e21a87b79522c0feb020
Natsu <62543597+Natsutoja@users.noreply.github.com>
2022-06-02 13:20:25 +0200

    Add files via upload

---
+++ TechnicUp/posting.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="postjob.css">
+        <html lang="pl">
+    </head>
+    <body>
+        <nav>
+            <div id="nav_links">
+                <a href="index.html">Strona główna</a>
+                <a href="">Zaoferuj Pracę</a>
+                <a href="">Szukaj Pracy</a>
+                <a href="contact.html">Kontakt</a>
+                <a href="">FAQ</a>
+            </div>
+        </nav>
+        <header class="postform_head">
+
+        </header>
+        <article class="longer_article">
+            <h1>Zaoferuj pracę</h1>
+
+                </div>
+                <div class="textone">
+                    <h2>Szukasz stażystów ? Pomożemy ci w tym !</h2>
+                    <p>
+                        Wypełnij ten prosty formularz, a my zajmiemy się resztą!
+                    </p>
+                    
+                    <form>
+                        <label for="fname">Imię</label><br>
+                        <input type="text" class="fname" name="fname"><br>
+                        <label for="lname">Nazwisko</label><br>
+                        <input type="password" class="lname" name="lname"><br>
+                        <label for="fname">Nazwa Firmy</label><br>
+                        <input type="text" class="fname" name="fname"><br>
+                        <label for="lname">Stanowisko/Wymagania</label><br>
+                        <input type="password" class="lname" name="lname"><br>
+                        <label for="lname">Dyplom Technika</label><br>
+                        <br>
+                        <select>
+                            <option>Technik Programista</option>
+                            <option>Technik Fryzjer</option>
+                            <option>Technik Elektryk</option>
+                            <option>Technik Leśnik</option>
+                        </select><br>
+                        <br>
+                        <label for="lname">Email</label><br>
+                        <input type="password" class="lname" name="lname"><br>
+                        <input type="submit" value="Wyślij Ofertę" class="logbut">
+                    </form>
+                </div>
+           
+        
+        </article>
+        <footer>
+            <h3>
+                Copyright TechnicUp 2022
+            </h3>
+        </footer>
+    </body>
+</html>
